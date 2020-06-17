514c03d2e4cc1b45e3fe2749f61c17493c248982
Jardel <jardel1101@gmail.com>
2020-06-17 03:22:55 -0300

    Repositories CRUD, Likes - Challenge GoStack

--- src/app.js
+++ src/app.js
@@ -1,33 +1,77 @@
-const express = require("express");
-const cors = require("cors");
+const express = require("express")
+const cors = require("cors")
+const { uuid, isUuid } = require("uuidv4")
 
-// const { uuid } = require("uuidv4");
+const app = express()
 
-const app = express();
+app.use(express.json())
+app.use(cors())
+app.use('/repositories/:id', validateId)
 
-app.use(express.json());
-app.use(cors());
+function validateId(req, res, next) {
+  const { id } = req.params
 
-const repositories = [];
+  if(!isUuid(id)) 
+    return res.status(400).json({ error: 'Invalid repository ID'})
+  
+  return next()
+}
 
-app.get("/repositories", (request, response) => {
-  // TODO
-});
+const repositories = []
 
-app.post("/repositories", (request, response) => {
-  // TODO
-});
+app.get("/repositories", (req, res) => {
+  res.json(repositories)
+})
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
-});
+app.post("/repositories", (req, res) => {
+  const { title, url, techs } = req.body 
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
-});
+  const repository = { id: uuid(), title, url, techs, likes: 0 }
+  repositories.push(repository)
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
-});
+  return res.json(repository)
+})
 
-module.exports = app;
+app.put("/repositories/:id", (req, res) => {
+  const { id } = req.params
+
+  const repIndex = repositories.findIndex(rep => rep.id === id)
+
+  if(repIndex < 0)
+    return res.status(400).json({error: "Repository not found"})
+
+  const { title, url, techs } = req.body
+
+  repositories[repIndex].title = title
+  repositories[repIndex].url   = url
+  repositories[repIndex].techs = techs
+  
+  return res.json(repositories[repIndex])
+})
+
+app.delete("/repositories/:id", (req, res) => {
+  const { id } = req.params
+  const repIndex = repositories.findIndex(rep => rep.id === id)
+
+  if(repIndex < 0)
+    return res.status(400).json({error: "Repository not found"})
+  
+  repositories.splice(repIndex, 1)
+
+  return res.status(204).json()
+})
+
+app.post("/repositories/:id/like", (req, res) => {
+  const { id } = req.params
+
+  const repository = repositories.find(rep => rep.id === id)
+
+  if(!repository)
+    return res.status(400).json({error: "Repository not found"})
+
+  repository.likes++
+
+  return res.status(200).json(repository)
+})
+
+module.exports = app
--- src/server.js
+++ src/server.js
@@ -1,3 +1,7 @@
-const app = require("./app");
+const app = require("./app")
 
-app.listen(3333);
+const port = 3333
+
+app.listen(port, () => {
+  console.log(`✨ Server started on port ${port}!!`)
+})
